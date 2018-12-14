29c65be51519f818bcf514328813a426dda7a801
Aleem Isiaka <aleemisiaka@gmail.com>
2018-12-14 13:10:01 +0100

    Completes init data seeding for roles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable no-console */
 // Start the server. (Can be re-generated.)
 // !code: preface // !end
@@ -9,10 +8,13 @@ const app = require('./app');
 
 const port = app.get('port');
 const server = app.listen(port);
-// !code: init2 // !end
+// !code: init2
+const seedInitData = require('./seed-init-data');
+// !end
 
 process.on('unhandledRejection', (reason, p) => {
-  // !<DEFAULT> code: unhandled_rejection_log
+  // !code: unhandled_rejection_log
+  console.log(reason);
   logger.error('Unhandled Rejection at: Promise ', p, reason);
   // !end
   // !code: unhandled_rejection // !end
@@ -20,9 +22,15 @@ process.on('unhandledRejection', (reason, p) => {
 
 server.on('listening', async () => {
   // !<DEFAULT> code: listening_log
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
+  logger.info(
+    'Feathers application started on http://%s:%d',
+    app.get('host'),
+    port
+  );
   // !end
-  // !code: listening // !end
+  // !code: listening
+  seedInitData(app);
+  // !end
   // !code: listening1 // !end
 });
 
---
+++ src/seed-init-data.js
@@ -0,0 +1,25 @@
+const seeder = async (service, data, findParams = {}, sameTotal = false) => {
+  let results = await service.find(findParams);
+  // If both retuls total and data length is false || result.total is not truthy
+  if (
+    (sameTotal && Array.isArray(data) && data.length !== results.total) ||
+    (!sameTotal && !results.total)
+  ) {
+    results = await service.create(data);
+  }
+  return results.data || results;
+};
+module.exports = async (app) => {
+  // Seeds Roles
+  const roleData = await seeder(
+    app.service('/roles'),
+    app.get('supportedRoles'),
+    {},
+    true
+  );
+  // Get the admin role _id
+  // eslint-disable-next-line
+  const { _id: adminRoleId } = await roleData.find(
+    (d) => d.name === 'Administrator'
+  );
+};
