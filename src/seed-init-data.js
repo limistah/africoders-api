76d82112aa176094b4437fcf3a3ad0f95c0e30d8
Aleem Isiaka <aleemisiaka@gmail.com>
2018-12-18 10:41:02 +0100

    Upgrades init data seeder

--- src/seed-init-data.js
+++ src/seed-init-data.js
@@ -1,8 +1,9 @@
 const seeder = async (service, data, findParams = {}, sameTotal = false) => {
   let results = await service.find(findParams);
   // If both retuls total and data length is false || result.total is not truthy
+  // checking for inequality to make sure that subsequent seeds do not run in case of the seeded document was deleted.
   if (
-    (sameTotal && Array.isArray(data) && data.length !== results.total) ||
+    (sameTotal && Array.isArray(data) && data.length < results.total) ||
     (!sameTotal && !results.total)
   ) {
     results = await service.create(data);
