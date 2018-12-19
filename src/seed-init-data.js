5fdfbd1b841ed8de969fe75cb445cb9f3587a341
Aleem Isiaka <aleemisiaka@gmail.com>
2018-12-19 15:02:21 +0100

    Allows seeding admin user

--- src/seed-init-data.js
+++ src/seed-init-data.js
@@ -23,4 +23,13 @@ module.exports = async (app) => {
   const { _id: adminRoleId } = await roleData.find(
     (d) => d.name === 'Administrator'
   );
+
+  let adminUser = app.get('adminUser');
+  const pass = adminUser.password;
+  adminUser.roleId = adminRoleId.toString();
+  adminUser = await seeder(app.service('/users'), adminUser, {username: adminUser.username});
+  // sets the data correctly
+  adminUser = adminUser.username ? adminUser : adminUser[0];
+  // eslint-disable-next-line
+  console.log(`User '${adminUser.username}:${pass}' has been created`);
 };
