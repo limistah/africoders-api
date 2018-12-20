b0982ef7220a1651d3aac82594148b0d631f73be
Aleem Isiaka <aleemisiaka@gmail.com>
2018-12-20 10:23:29 +0100

    Updates API Docs host based on host configuration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = require('./app');
 // !code: imports // !end
 // !code: init // !end
 
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 const server = app.listen(port);
 // !code: init2
 const seedInitData = require('./seed-init-data');
@@ -28,7 +28,16 @@ server.on('listening', async () => {
   // !code: listening
   seedInitData(app);
   // !end
-  // !code: listening1 // !end
+  // !code: listening1
+  const host = app.get('host');
+  let swaggerConfig = require('./../public/swagger-ui/config.v1.json');
+  if (swaggerConfig.host !== host) {
+    swaggerConfig.host = host;
+    require('fs').writeFile(`${process.cwd()}/public/swagger-ui/config.v1.json`, JSON.stringify(swaggerConfig), (err) => {
+      if (err) throw err;
+    });
+  }
+  // !end
 });
 
 // !code: funcs // !end
